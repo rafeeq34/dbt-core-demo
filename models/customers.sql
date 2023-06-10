1ce1011b3b2ad5738abef8138259fdc780e2b0cf
rmoham26 <rmoham26@ford.com>
2023-06-10 23:12:32 +0530

    add changes to main

---
+++ models/customers.sql
@@ -0,0 +1,24 @@
+{{
+config(materialized='table')
+}}
+with customers as (
+
+select * from {{ref('stg_customers')}}
+
+),
+
+orders as (
+
+select * from {{ref('stg_orders')}}
+
+),
+
+customer_orders as (
+SELECT
+customer_id,
+min(order_date) as first_order_date,
+max(order_date) as most_recent_order_date,
+count(order_id) as number_of_orders
+from orders
+group by 1)
+select * from customer_orders
---
+++ models/stg_customers.sql
@@ -0,0 +1,5 @@
+SELECT
+id as customer_id,
+first_name,
+last_name
+from `dbt-tutorial`.jaffle_shop.customers
